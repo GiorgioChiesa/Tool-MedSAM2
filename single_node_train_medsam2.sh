bddd673c967507155b537946c11de0f832e77290
chiesa <giorgio.chiesa@unito.it>
2025-12-16 22:52:12 +0000

    Pronto

--- single_node_train_medsam2.sh
+++ single_node_train_medsam2.sh
@@ -3,7 +3,7 @@ export PATH=/usr/local/cuda/bin:$PATH
 # make sure the checkpoint is under `MedSAM2/checkpoints/sam2.1_hiera_tiny.pt`
 config=configs/sam2.1_hiera_tiny_finetune512.yaml
 
-output_path=/scratch/SAMS/MedSAM2v2/MedSAM2/exp_log/Provolia
+output_path=/scratch/SAMS/MedSAM2v2/MedSAM2/exp_log/Provolone
 
 # Function to run the training script
 CUDA_VISIBLE_DEVICES=0,1 python3 training/train.py \
